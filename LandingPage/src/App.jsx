46bf643361caf8e2eb2c62c6adc5420d858f19fd
shaurya35 <shauryajha35@gmail.com>
2024-04-05 02:28:02 +0530

    add header

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      hi
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
