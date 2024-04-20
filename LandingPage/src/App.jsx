fcd3db969e826ab31fd242b1ee0480cae8f93674
shaurya35 <shauryajha35@gmail.com>
2024-04-20 22:56:21 +0530

    add spacing\

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
