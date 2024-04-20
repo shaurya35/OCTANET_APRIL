bb2b240c33b7d9d62a5657c603307f65343d8ec3
shaurya35 <shauryajha35@gmail.com>
2024-04-20 23:02:04 +0530

    remove spacing

--- To-do List/src/App.jsx
+++ To-do List/src/App.jsx
@@ -73,28 +73,13 @@ function App() {
         <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className="text-2xl">So, what you're upto today?</h2>
           <div className="flex">
-            <input
-              onChange={handleChange}
-              value={todo}
-              type="text"
-              className="w-full rounded-full px-5 py-1"
-            />
-            <button
-              onClick={handleAdd}
-              disabled={todo.length <= 3}
-              className="bg-violet-800 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-4 py-2 text-sm font-bold text-white"
-            >
+            <input onChange={handleChange} value={todo} type="text" className="w-full rounded-full px-5 py-1"/>
+            <button onClick={handleAdd} disabled={todo.length <= 3} className="bg-violet-800 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-4 py-2 text-sm font-bold text-white" >
               Add
             </button>
           </div>
         </div>
-        <input
-          className="my-4"
-          id="show"
-          onChange={toggleFinished}
-          type="checkbox"
-          checked={showFinished}
-        />
+        <input className="my-4" id="show" onChange={toggleFinished} type="checkbox" checked={showFinished} />
         <label className="mx-2 " htmlFor="show">
           Display Finished
         </label>
@@ -107,22 +92,13 @@ function App() {
               (showFinished || !item.isCompleted) && (
                 <div key={item.id} className={"todo flex my-3 justify-between"}>
                   <div className="flex gap-5">
-                    <input
-                      name={item.id}
-                      onChange={handleCheckbox}
-                      type="checkbox"
-                      checked={item.isCompleted}
-                      id=""
-                    />
+                    <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
                     <div className={item.isCompleted ? "line-through" : ""}>
                       {item.todo}
                     </div>
                   </div>
                   <div className="buttons flex h-full">
-                    <button
-                      onClick={(e) => handleEdit(e, item.id)}
-                      className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                    >
+                    <button onClick={(e) => handleEdit(e, item.id)} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1" >
                       <FaEdit />
                     </button>
                     <button
