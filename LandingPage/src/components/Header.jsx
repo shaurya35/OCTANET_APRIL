7fd4d613dbbeb77900c89941af3a033066d8a2d6
shaurya35 <shauryajha35@gmail.com>
2024-04-20 22:57:40 +0530

    add spacing

--- LandingPage/src/components/Header.jsx
+++ LandingPage/src/components/Header.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-import "./Header.css"
+import React from "react";
+import logo from "../assets/logo.png";
+import "./Header.css";
 const Header = () => {
   return (
-    <header className='header'>
-        <a href="#" className="logo">
-            <img src={logo} alt="Logo" />
+    <header className="header">
+      <a href="#" className="logo">
+        <img src={logo} alt="Logo" />
+      </a>
+
+      <nav className="navbar">
+        <div className="fas fa-times" id="close"></div>
+        <a href="#" className="nav_item">
+          Home
         </a>
-        
-        <nav className="navbar">
-          <div className="fas fa-times" id='close'></div>
-          <a href="#" className="nav_item">Home</a>
-          <a href="#" className="nav_item">About</a>
-          <a href="#" className="nav_item">Menu</a>
-          <a href="#" className="nav_item">Contact</a>
-        </nav>
+        <a href="#" className="nav_item">
+          About
+        </a>
+        <a href="#" className="nav_item">
+          Menu
+        </a>
+        <a href="#" className="nav_item">
+          Contact
+        </a>
+      </nav>
 
-        <div className="fas fa-bars"></div>
+      <div className="fas fa-bars"></div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
