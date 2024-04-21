0c765f8752740efd124b5b249147aba9fd677cc1
shaurya35 <shauryajha35@gmail.com>
2024-04-21 21:56:21 +0530

    space

--- To-do List/src/App.jsx
+++ To-do List/src/App.jsx
@@ -101,8 +101,7 @@ function App() {
                     <button onClick={(e) => handleEdit(e, item.id)} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1" >
                       <FaEdit />
                     </button>
-                    <button
-                      onClick={(e) => {
+                    <button onClick={(e) => {
                         handleDelete(e, item.id);
                       }}
                       className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
